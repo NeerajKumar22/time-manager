c9abbe9b7b0fbe4ed9c3c76816bc7458411dbca5
NeerajKumar22 <ineerajkr@gmail.com>
2019-08-23 19:23:33 +0530

    add route handler

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// require the modules.....
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,14 +9,17 @@ var helmet = require('helmet');
 var expressStaticGzip = require("express-static-gzip");
 var mongoose = require('mongoose');
 
+
 var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api')
+// var apiRouter = require('./routes/api')
+var usersRouter = require('./routes/api/users')
 
 var app = express();
 
 app.use(helmet());
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -38,16 +43,21 @@ app.use('/dist/bundle', expressStaticGzip(path.join(__dirname, 'dist/bundle'), {
   }
 }));
 
+
 // fix depreciation warning.
 mongoose.set('useFindAndModify', false);
 mongoose.set('useNewUrlParser', true);
 
+
 // connect to mongodb
-mongoose.connect('mongodb://localhost:27017/node_react_bp', function (err) {
+
+mongoose.connect('mongodb://localhost:27017/time_manager', function (err) {
   console.log('mongodb connected ?', err ? false : true);
 });
 
+
 // webpack
+
 if (process.env.NODE_ENV === "development") {
   var webpack = require("webpack");
   var webpackConfig = require("./webpack.config");
@@ -63,23 +73,32 @@ if (process.env.NODE_ENV === "development") {
   app.use(require("webpack-hot-middleware")(compiler));
 }
 
+
 // Route handler
-app.use('/api/v1', apiRouter) // api route handler
+
+//app.use('/api/v1', apiRouter) // api route handler
+
 app.use('/', indexRouter); // react handler
+app.use("/users/", usersRouter);
 
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
+
 app.use(function(err, req, res, next) {
+
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
