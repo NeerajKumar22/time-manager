d53322e2ea626e47ca75ecd3b015cc7c32854133
NeerajKumar22 <ineerajkr@gmail.com>
2019-08-25 22:17:22 +0530

    add routing for navbar

--- client/containers/App.js
+++ client/containers/App.js
@@ -7,6 +7,9 @@ import { getCurrentUser, noToken } from '../actions'
 
 
 import HomePage from '../components/home/HomePage';
+import NavBar from '../components/navbar/NavBar'
+import Register from '../components/navbar/Register'
+import Login from '../components/navbar/Login'
 
 class App extends Component {
   state = { 
@@ -27,6 +30,9 @@ class App extends Component {
     return (
       <div>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/navbar" component={NavBar} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
     );
   }
