dcede0175cb506d147773b6fc90b6704b115f587
NeerajKumar22 <ineerajkr@gmail.com>
2019-08-26 21:35:05 +0530

    update

--- client/components/navbar/NavBar.js
+++ client/components/navbar/NavBar.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import Register from "./Register"
-import Login from "./Login"
 
 
 const NavBar = () => {
   return (
     <div>
       <h1>NavBar Page</h1>
-      {/* <Register /> */}
-      {/* <Login /> */}
     </div>
   );
 }
